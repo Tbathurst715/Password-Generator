8172b9e00547179f71bfcc33f3e954debf533f0d
Tbathurst715 <timbathurst@yahoo.com>
2022-07-24 20:13:58 -0600

    updated data to array

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,52 @@
 
 var generateBtn = document.querySelector("#generate");
 
-var lowCase = confirm("Would you like lowercase letters in this password?");
-var upperCase = confirm("Would you like uppercase letters in this password?");
-var nums = confirm("Do you want numbers in this password?");
-var symbols = confirm("Do you want symbols in this password?");
-var passLength = prompt("Enter the length you would like your password to be anywhere between 8 to 128 characters");
-  
-
-function passwordReq () {
-  if (passLength < 8 || passLength > 128) return alert("Must be between 8 and 128 characters");
-  if (lowCase === false && upperCase === false && nums === false && symbols === false) return alert("Please choose at least one option");
-}
-
-var characters = "";
-
 var letters = "abcdefghijklmnopqrstuvwxyz";
+var lettersArray = letters.split(" ");
+var lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lettersUpperArray = lettersUpper.split(" ");
 var numbers = "0123456789";
-var symbols = "!@#$%^&*()*+-./";
-
-function generatePassword() {
-  if (lowCase === true) {
-    characters = letters
-  }
-  if (upperCase === true) {
-    characters = letters.toUpperCase
-  }
-  if (numbers === true) {
-    characters = numbers
-  }
-  if (symbols === true) {
-    characters = symbols
-  }
-}
+var numbersArray = numbers.split(" ");
+var specialCharacters = "!@#$%^&*";
+var specialCharactersArray = specialCharacters.split(" ");
 
 
-
-
-
-
-
-
-
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var generatePassword = "";
+  var characters = [];
   var passwordText = document.querySelector("#password");
+
+  var passLength = prompt("Enter the length you would like your password to be anywhere between 8 to 128 characters");
+  if (passLength < 8 || passLength > 128) {
+    alert("Must be between 8 and 128 characters");
+  }
+  else {
+
+    if (confirm("Would you like lowercase letters in your password?") === true) { Array.prototype.push(characters, lettersArray); };
+
+    if (confirm("Do you want to Uppercase letters in your password?") === true) { Array.prototype.push(characters, lettersUpperArray); };
+
+    if (confirm("Would you like numbers in your password?") === true) { Array.prototype.push(characters, numbersArray); };
+
+    if (confirm("Do you want special characters in your password?") === true) { Array.prototype.push(characters, specialCharactersArray); };
+
+    if (characters === 0) {
+      alert("You must select one option");
+    }
+    else {
+      for (var i = 0; i < passLength; i++) {
+        var random = Math.floor(Math.random() * characters.length); generatePassword = characters[random];
+
+        console.log(generatePassword);
+      }
+    }
+  }
+  var password = characters;
 
   passwordText.value = password;
 
+  passwordText.innerHTML = generatePassword;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+generateBtn.addEventListener("click", writePassword)
